0886bc249ced33bac6127215e4e704e2e63cd0c1
zizi.liu <zizi.liu@dianping.com>
2015-11-03 09:43:32 +0800

    init

---
+++ src/main/java/com/hzdp/entity/UserAccount.java
@@ -0,0 +1,42 @@
+package com.hzdp.entity;
+
+public class UserAccount {
+
+	private int id;
+	private String name;
+	private String mobile;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
