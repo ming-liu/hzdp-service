f3e27a68d6248d4fc77ad6351b5fa97fae959745
zizi.liu <zizi.liu@dianping.com>
2015-11-06 07:21:20 +0800

    init

---
+++ src/main/java/com/hzdp/dao/BaseDao.java
@@ -0,0 +1,10 @@
+package com.hzdp.dao;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+public class BaseDao {
+
+//	@Resource(name = "")
+//	@javax.annotation.R
+	private SqlMapClient sqlMapClient;
+}
